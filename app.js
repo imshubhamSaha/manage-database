4d95c6000f1ae5f0acce22f826118710da170fa7
Shubham Saha <imshubhamsaha@gmail.com>
2025-04-23 22:47:03 +0530

    checking if server and db connection is working well

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const initializeDatabase = require("./util/db-connection");
+require("dotenv").config();
 const app = express();
-const PORT = 3000;
-app.use(express.json);
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+initializeDatabase((err, db) => {
+  if (err) {
+    console.error("Database initialization failed:", err);
+    process.exit(1);
+  }
+
+  app.get("/", (req, res) => {
+    res.send("app running");
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
 });
