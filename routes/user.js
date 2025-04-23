ab279cae3ce62d26fd29e099d321797e41774d93
Shubham Saha <imshubhamsaha@gmail.com>
2025-04-23 22:52:48 +0530

    added more routes for getting data

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/usersController");
 
-router.post("/", userController.createUser);
-router.put("/:id", userController.updateUser);
-router.delete("/:id", userController.deleteUser);
+// Routes for users
+router.post("/", userController.createUser); // Create new user
+router.get("/", userController.getAllUsers); // Get all users
+router.get("/:id", userController.getUserById); // Get user by ID
+router.put("/:id", userController.updateUser); // Update user by ID
+router.delete("/:id", userController.deleteUser); // Delete user by ID
 
 module.exports = router;
